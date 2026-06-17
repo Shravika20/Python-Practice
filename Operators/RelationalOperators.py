023a2d21b6ec9f6a653391a5e882324b68fe7b58
Shravika <shravika2056@gmail.com>
2026-06-17 09:57:09 +0530

    Add RelationalOperators program

---
+++ Operators/RelationalOperators.py
@@ -0,0 +1,6 @@
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+print("Equal:", a == b)
+print("Not Equal:", a != b)
+print("Greater Than:", a > b)
+print("Less Than:", a < b)
