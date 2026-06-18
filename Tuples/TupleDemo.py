d79b3cb95db45721ad281b704cada85cc9ecc106
Shravika <shravika2056@gmail.com>
2026-06-18 10:11:22 +0530

    Add TupleDemo

---
+++ Tuples/TupleDemo.py
@@ -0,0 +1,7 @@
+# Tuple Demo
+fruits = ("Apple", "Banana", "Mango", "Orange")
+print("Tuple Elements:")
+for fruit in fruits:
+    print(fruit)
+print("First Element:", fruits[0])
+print("Number of Elements:", len(fruits))
