0fc5d50f6ec8cc64f96ab2bc05f69be015646667
Shravika <shravika2056@gmail.com>
2026-06-19 10:27:21 +0530

    Add Readfile

---
+++ FileHandling/ReadFile.py
@@ -0,0 +1,8 @@
+try:
+    file = open("student.txt", "r")
+    data = file.read()
+    print("File Content:")
+    print(data)
+    file.close()
+except FileNotFoundError:
+    print("File does not exist.")
