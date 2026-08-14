4adf0a47582c3fb7398bf912732389f336872877
Shravika <shravika2056@gmail.com>
2026-08-15 05:28:36 +0530

    Add wordfrequency program

---
+++ FileHandling/WordFrequency.py
@@ -0,0 +1,12 @@
+from collections import Counter
+filename = input("Enter file name: ")
+try:
+    with open(filename, "r") as file:
+        text = file.read().lower()
+    words = text.split()
+    word_count = Counter(words)
+    print("\nWord Frequency:")
+    for word, count in word_count.most_common():
+        print(f"{word}: {count}")
+except FileNotFoundError:
+    print("File not found.")
